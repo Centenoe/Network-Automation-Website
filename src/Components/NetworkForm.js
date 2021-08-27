2f1eb1d37e9773f47c0c5fbf4cc856765e6849be
Penguinkred <Penguinkred@protonmail.com>
2021-08-27 00:16:05 -0500

    Added cool console log output and it "works"

--- src/Components/NetworkForm.js
+++ src/Components/NetworkForm.js
@@ -10,62 +10,78 @@ import { render } from '@testing-library/react';
 import Information from './Information';
 //import "./styles.css";
 
-
-
 export default function App() {
-    const { register, handleSubmit, formState: { errors } } = useForm();
-
-    // const onSubmit = data => <DisplayForm data = {data}/>
+    const { register, handleSubmit, formState: { errors } } = useForm(
+        
+        {
+        defaultValues: {
+            Hostname: "test",
+            Enable_Secret: "Enable",
+            }
+        });
     const onSubmit = data => {
-        let pleasework = data;
-       return pleasework
-    }
-        console.log(errors);
+         {/* <Information userdata = {data}/> */}
+        console.log(data)
+        console.log(data.Hostname)
+        console.log(data.Domain_Lookup)
+        console.log(data.Enable_Secret)
+        console.log(data.Logging_Synchronous)
 
-        console.log(onSubmit())
+        let data_array = [data]
+
+        return console.log(data_array)
     
-                return (
+    };
+    
+    console.log(onSubmit())
+    let testingggg =  []
+    // for(let item of onSubmit){
+    //     console.log(item)
+    // }
+    console.log("Look here you little",)
+    console.log(errors);
 
-            <form onSubmit={handleSubmit(onSubmit)}>
+    return (
 
-                <Grid container spacing={1}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+                <p>{onSubmit} etsaete</p>
+            <Grid container spacing={1}>
 
-                    <Grid item xs={12}>
-                        <TextField type="text" {...register("Hostname", { required: true, maxLength: 15 })} id="outlined-basic" label="Hostname" variant="outlined" />
-                    </Grid>
-
-                    <Grid item xs={12}>
-                        <FormControl>
-                            <InputLabel id="Domain Lookup">Domain Lookup</InputLabel>
-                            <Select {...register("Domain Lookup")}>
-                                <option value="Yes">Yes</option>
-                                <option value="No">No</option>
-                            </Select>
-                        </FormControl>
-                    </Grid>
-
-                    <Grid item xs={12}>
-                        <TextField type="text" {...register(" Enable Secret", {})} id="outlined-basic" label="Enable Secret" variant="outlined" />
-                    </Grid>
-
-                    <Grid item xs={12}>
-                        <FormControl>
-                            <InputLabel>Logging Synchronous</InputLabel>
-                            <Select {...register("Logging Synchronous")}>
-                                <option value="Enable">Enable</option>
-                                <option value="Disable">Disable</option>
-                            </Select>
-                        </FormControl>
-                    </Grid>
+                <Grid item xs={12}>
+                    <TextField type="text" {...register("Hostname", { required: true, maxLength: 15 })} id="outlined-basic" label="Hostname" variant="outlined"/>
                 </Grid>
 
-                {/* <Information userdata = {data}/> */}
+                <Grid item xs={12}>
+                    <FormControl>
+                        <InputLabel id="Domain Lookup">Domain Lookup</InputLabel>
+                        <Select {...register("Domain_Lookup")}>
+                            <option value="Yes">Yes</option>
+                            <option value="No">No</option>
+                        </Select>
+                    </FormControl>
+                </Grid>
 
-                <p>{onSubmit.Hostname} </p>
+                <Grid item xs={12}>
+                    <TextField type="text" {...register("Enable_Secret", {})} id="outlined-basic" label="Enable Secret" variant="outlined"/>
+                </Grid>
 
-                
-                <input type="submit" value="submit" />
+                <Grid item xs={12}>
+                    <FormControl>
+                        <InputLabel>Logging Synchronous</InputLabel>
+                        <Select {...register("Logging_Synchronous")}>
+                            <option value="Enable">Enable</option>
+                            <option value="Disable">Disable</option>
+                        </Select>
+                    </FormControl>
+                </Grid>
+                {/* <p>{onSubmit.data.Hostname}</p> */}
+            </Grid>
 
-            </form>
-        );
-    }
+            {/* <Information userdata = {data}/> */}
+
+          {/* <p>{onSubmit.Hostname} </p> */}
+            <input type="submit" value="submit" />
+
+        </form>
+    );
+}
